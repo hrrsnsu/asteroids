ae4736b9129e5e1e691261a463d8d9c4cee9f03f
hrrsnsu <hrrsnsu@gmail.com>
2025-07-05 18:05:18 -0700

    added player and movement, switched to groups for object usage

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import pygame # type: ignore
 from constants import * 
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     pygame.display.init()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.container = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT / 2)
+    updatable.add(player)
+    drawable.add(player)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        
+        for item in drawable:
+            item.draw(screen)
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
+        updatable.update(dt)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        
 
 if __name__ == "__main__":
     main()
